73556cf6c2b709926ef7598df7366c1773755f0d
Alok Kumar <99880162+alokhit@users.noreply.github.com>
2022-03-29 09:15:51 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,192 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	  /*  for(int i=1; i<=5; i++){             // i=1,2,3
+	        for(int j=1; j<=5; j++){          // j=1,2,3,4,5,(6 NOt print)
+	            System.out.print(j);          //  =1,2,3,4,5,
+	        }                                // = 1,2,3,4,5     	                                                     
+	        System.out.println();
+	        }*/
+	      /*  for(int i=1; i<=5; i++){
+	            for(int j=1; j<=5; j++){
+	                System.out.print(i);
+	            }
+	            System.out.println();
+	        } */
+	        
+	     /*   for(int i=1; i<=5; i++){                         // i =1,2
+	            for(int j=1; j<=i; j++){                     // j= 1
+	            for(int j=1; j<=i; j++){                     // j= 1
+	                System.out.print("$ ");
+	            }
+	         System.out.println();
+	    } */
+	    
+	/*    for(int i=1; i<=5; i++){
+	        for(int j=1; j<=i; j++){
+	            System.out.print(j +" ");
+	        }
+	       System.out.println();
+	    } */
+	    
+     /*	     for(int i=1; i<=5; i++){
+	         for(int j=1; j<=i; j++){
+	             System.out.print(i+" ");
+	         }
+	         System.out.println();
+	     }   */
+	 /*    for(int i=1; i<=5; i++){
+	        for(int k=0; k<=5; k++){
+	            System.out.print(" ");
+	        }
+	         for(int j=1; j<=5; j++){
+	             System.out.print("* ");
+	         }
+	         System.out.println();   
+	         
+	     }  */
+	    /* for(int i=1; i<=5; i++){
+	         for(int k=1; k<=5; k++){
+	             System.out.print("        ");
+	         }
+	         for(int j=1; j<=i; j++){
+	             System.out.print("* ");
+	         }
+	         System.out.println();
+	     } */
+	    /*  for(int i=1; i<=5; i++){
+	         for(int k=1; k<=5; k++){
+	             System.out.print(" ");
+	         }
+	         for(int j=1; j<=5; j++){
+	             System.out.print(j);
+	         }
+	         System.out.println();    
+	     }*/
+	     
+	  /*   for(int i=1; i<=5; i++){
+	         for(int k=1; k<=5; k++){
+	             System.out.print("        ");
+	         }
+	         for(int j=1; j<=i; j++){
+	             System.out.print(j);
+	         }
+	         System.out.println();
+	     }  */
+	     
+	  /*   for(int i=1; i<=5; i++){
+	         for(int k=1; k<=i; k++){
+	             System.out.print(" ");
+	         }
+	         for(int j=1; j<=5; j++){
+	             System.out.print("*");
+	     }
+	     System.out.println();
+	}              */
+	
+	 /*  for(int i=1; i<=5; i++){
+	       for(int k=1; k<=i; k++){
+	           System.out.print(" ");
+	       }
+	       for(int j=1; j<=i; j++){
+	           System.out.print("*");
+	       }
+	         System.out.println(); 
+	   }   */
+	   
+	/*   for(int i=1; i<=5; i++){
+	       for(int j=5; j>=i; j--){
+	           System.out.print("*");
+	       }
+	       System.out.println();
+	   }  */
+	  /* for( int i=5; i>=1; i--){
+	       for(int j=1; j<=i; j++ ){     //i= 5,4,3,2,1,!0
+	           System.out.print("*");    // j=5,!4
+	       }                             //  =5,4,!3
+	       System.out.println();         //  =5,4,3,!2
+	   //}         */                    //  =5,4,3,2,!1
+	   
+	   /*  for(int i=1; i<=5; i++){
+	       for(int j=5; j>=i; j--){
+	           System.out.print(j+" ");
+	       }
+	       System.out.println();
+	   }  */
+/*	   for(int i=5; i>=1; i--){
+	       for(int j=1; j<=i; j++){
+	           System.out.print(j+" ");
+	       }
+	       System.out.println();
+	   }  */
+	   
+   /*	   for(int i=1; i<=5; i++){
+	 
+	       for(int k=1; k<=5; k++){
+	       System.out.print(" ");
+	      }
+	        for(int j=5; j>=i; j--){
+	        System.out.print("*");
+	        }     
+	   System.out.println();
+	}    */
+	
+/*   	for(int i=1; i<=5; i++){     //QN
+	    for(int k=1; k<=5; k++){
+	        System.out.print(" ");
+	    }
+	    for(int j=5; j>=i; j--){
+	        System.out.print("*");
+	    }
+	    System.out.println();
+	}
+	for(int i=1; i<=5; i++){
+	    for(int k=1; k<=5; k++){
+	        System.out.print(" ");
+	    }
+	    for(int j=1; j<=i; j++){
+	        System.out.print("*");
+	    }
+	    System.out.println();
+	}
+	*/
+ /*	for(int i=1; i<=5; i++){
+	    for(int k=5; k>=i; k--){
+	        System.out.print(" ");
+	    }
+	    for(int j=1; j<=5; j++){
+	        System.out.print("*");
+	    } 
+	    System.out.println();
+	}  */
+	
+/*	for(int i=1; i<=5; i++){
+	    for(int k=1; k<=i; k++){
+	        System.out.print(" ");
+	    }
+	    for(int j=1; j<=5; j++){
+	        System.out.print("*");
+	    }
+	    System.out.println();
+	}
+	
+ 	for(int i=1; i<=5; i++){
+	 for(int k=5; k>=i; k--){
+	   System.out.print(" "); 
+	}
+	for(int j=1; j<=5; j++){
+	    System.out.print("*");
+	}
+	System.out.println();
+  }    */
+ 
+}
+}
